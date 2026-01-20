4a8b16c9d295238ef8db01e9cd7641fcdaf4144a
MoonSsugar <hirnyjt@gmail.com>
2026-01-20 17:12:21 +0100

    redesign cart button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router";
 import { useAppSelector } from "../redux/hooks";
+import { IoCartOutline } from "react-icons/io5";
 
 export default function Header() {
   const navigator = useNavigate();
@@ -9,11 +10,19 @@ export default function Header() {
   const totalQuantity = items.reduce((sum, item) => sum + item.quantity, 0)
 
   return (
-    <header className="flex justify-end bg-cyan-300 p-1">
+    <header className="flex justify-end bg-gray-300 p-1">
       <button
+        className="relative mr-5 p-1"
         onClick={() => navigator("/cart")}
       >
-        Cart {totalQuantity > 0 ? totalQuantity : ""}
+        <IoCartOutline size={35} /> 
+
+        {totalQuantity > 0 && (
+          <span 
+            className="absolute -top-1 -right-1 bg-red-300 w-5 h-5 flex justify-center items-center rounded-full shadow-sm text-[10px]"
+          >{totalQuantity > 99 ? "99+" : totalQuantity}</span> 
+        )}
+
       </button>
     </header>
   );
